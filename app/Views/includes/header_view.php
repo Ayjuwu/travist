9445046bbd30393593b9ebc911898c591617baae
ayjuwu <raspmat676@gmail.com>
2025-02-19 21:41:26 +0100

    Suppression intégration code jQuery

--- app/Views/includes/header_view.php
+++ app/Views/includes/header_view.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php echo base_url() . '/assets/style.css'; ?>">
         <title> <?php echo esc($title); ?> </title>
-        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
